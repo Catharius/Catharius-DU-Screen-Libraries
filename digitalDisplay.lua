2e35738bb461d66206cf3b092a5edfda4fcbc267
jppra <jp.pradelles@adelya.com>
2022-05-26 21:17:27 +0200

    Add loop for making multiples counters

--- digitalDisplay.lua
+++ digitalDisplay.lua
@@ -5,27 +5,45 @@ if not DigitalDisplay then
     DigitalDisplay = {}
     DigitalDisplay.__index = DigitalDisplay
 
-    function DigitalDisplay:new(x, y, size, maxdigit, r, g, b)
+    function DigitalDisplay:new(x, y, size, maxdigit, r, g, b, backcolorRatio)
 
         local self = {
             x = x or 0,
             y = y or 0,
-            size = size or 100,
+            size = size or 1,
             maxdigit = maxdigit or 1,
             r = r or 1,
             g = g or 0,
-            b = b or 0
+            b = b or 0,
+            backcolorRatio = backcolorRatio or 0.09,
+            horizontalLineWidth = 10*size,
+            horizontalLineHeight = 4*size,
+            triangleWidth = 4*size,
+            verticalLineWidth = 4*size,
+            verticalLineHeight = 10*size,
+            lineSpacing = 1*size
         }
         function self:draw(layer)
-            local x, y, size, maxdigit, r, g, b = self.x, self.y, self.size, self.maxdigit, self.r, self.g, self.b
-            horizontalLineWidth = 10*size
-            horizontalLineHeight = 4*size
-            triangleWidth = 4*size
-            verticalLineWidth = horizontalLineHeight
-            verticalLineHeight = horizontalLineWidth
-            lineSpacing = 1*size
+            local size, r, g, b, horizontalLineWidth, horizontalLineHeight, triangleWidth, verticalLineWidth, verticalLineHeight, lineSpacing = self.size, self.r, self.g, self.b, self.horizontalLineWidth, self.horizontalLineHeight, self.triangleWidth, self.verticalLineWidth, self.verticalLineHeight, self.lineSpacing
+            xoffset=0
+            yoffset=0
+            for i = 1, maxdigit, 1 do
+                self:drawDigit(layer,x+xoffset,y);
+                xoffset=xoffset+(triangleWidth*2)+horizontalLineWidth+(triangleWidth*2)+(lineSpacing*2)
+            end
+
+        end
+
+        function self:drawDigit(layer,x,y)
+            local size, r, g, b, horizontalLineWidth, horizontalLineHeight, triangleWidth, verticalLineWidth, verticalLineHeight, lineSpacing = self.size, self.r, self.g, self.b, self.horizontalLineWidth, self.horizontalLineHeight, self.triangleWidth, self.verticalLineWidth, self.verticalLineHeight, self.lineSpacing
             defaultx=x
             defaulty=y
+            r=r*backcolorRatio
+            g=g*backcolorRatio
+            b=b*backcolorRatio
+            defaultr=r
+            defaultg=g
+            defaultb=b
 
             -- A segment
             setNextFillColor(layer, r, g, b, 1)
@@ -92,6 +110,12 @@ if not DigitalDisplay then
             addTriangle(layer, x+horizontalLineWidth, y, x+horizontalLineWidth, y+horizontalLineHeight, x+horizontalLineWidth+(triangleWidth/2), y+(horizontalLineHeight/2))
             setNextFillColor(layer, r, g, b, 1)
             addTriangle(layer, x, y, x, y+horizontalLineHeight, x-triangleWidth/2, y+(horizontalLineHeight/2))
+
+            -- point
+            setNextFillColor(layer, r, g, b, 1)
+            x=defaultx+horizontalLineWidth+(triangleWidth*2)+lineSpacing
+            y=defaulty+horizontalLineHeight*2+verticalLineHeight*2+(triangleWidth*2)+(lineSpacing)
+            addCircle(layer, x, y, triangleWidth/2)
         end
 
         return setmetatable(self, DigitalDisplay)
@@ -101,7 +125,7 @@ end
 --# Getting resolution
 local rx, ry = getResolution()
 if not _init then
-    testDisplay = DigitalDisplay:new(rx/2, 10, 10, 1, 1, 0, 0)
+    testDisplay = DigitalDisplay:new(rx/2, 10, 3, 3, 1, 0, 0,0.09)
     _init = true
 end
 local testlayer = createLayer()
