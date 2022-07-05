e1aefa08b82ef9d385e4e1f760703f152361ee3d
Catharius <72415020+Catharius@users.noreply.github.com>
2022-07-05 23:11:52 +0200

    Cleaning code and manage big labels and sum values

--- donutChart/donutChart.lua
+++ donutChart/donutChart.lua
@@ -6,8 +6,6 @@ if not DonutChart then
             x = posX,
             y = posY,
             circleDiameter = circleDiameter,
-            data = dataLuaArray,
-            graphType = graphType,
             dataUnit = dataUnit or '',
             colorList = { { 65 / 255, 133 / 255, 244 / 255 }, { 233 / 255, 67 / 255, 53 / 255 }, { 250 / 255, 188 / 255, 4 / 255 }, { 51 / 255, 168 / 255, 83 / 255 }, { 255 / 255, 108 / 255, 1 / 255 }, { 71 / 255, 189 / 255, 198 / 255 }, { 123 / 255, 171 / 255, 247 / 255 }, { 241 / 255, 123 / 255, 114 / 255 }, { 252 / 255, 208 / 255, 80 / 255 }, { 112 / 255, 194 / 255, 135 / 255 }, { 255 / 255, 154 / 255, 77 / 255 }, { 125 / 255, 209 / 255, 215 / 255 }, { 179 / 255, 207 / 255, 251 / 255 }, { 243 / 255, 180 / 255, 174 / 255 }, { 254 / 255, 228 / 255, 155 / 255 }, { 173 / 255, 220 / 255, 186 / 255 }, { 255 / 255, 198 / 255, 153 / 255 }, { 180 / 255, 229 / 255, 232 / 255 }, { 233 / 255, 243 / 255, 254 / 255 }, { 254 / 255, 236 / 255, 235 / 255 } },
             currentCentralMenu = 1
@@ -20,20 +18,21 @@ if not DonutChart then
             local centralLayer = createLayer()
             local labelLayer = createLayer()
 
+            local smallFont = loadFont("Play", tonumber(string.format('%.0f', (self.circleDiameter * 3 / 100))))
             local font = loadFont("Play", tonumber(string.format('%.0f', (self.circleDiameter * 4 / 100))))
             local mediumFont = loadFont("Play", tonumber(string.format('%.0f', (self.circleDiameter * 6 / 100))))
             local bigFont = loadFont("Play", tonumber(string.format('%.0f', (self.circleDiameter * 5 / 100))))
 
             local circleRadius = self.circleDiameter / 2
-            local mediumLabelY=self.y-circleRadius/3
-            local labelWSpacing = self.circleDiameter/12
+            local mediumLabelY = self.y - circleRadius / 3
+            local labelWSpacing = self.circleDiameter / 12
             local labelHSpacing = 2
             local pi180 = math.pi / 180
 
             local theta = pi180 * 270 -- This allow to rotate the pie
             local dataSum = 0
-            local dataMin = {-1,-1,""}
-            local dataMax = {-1,-1,""}
+            local dataMin = { -1, -1, -1, "" }
+            local dataMax = { -1, -1, -1, "" }
 
             -- Get the first row of the data array (it is the column header of the data)
             local firstRow = dataLuaArray[1]
@@ -69,15 +68,17 @@ if not DonutChart then
                     if dataLabelWidth > biggestLabelWidth then
                         biggestLabelWidth = dataLabelWidth
                     end
-                    if dataMin[1] > data or dataMin[1]<0 then
+                    if dataMin[1] > data or dataMin[1] < 0 then
                         dataMin[1] = data
-                        dataMin[2] = dataLabelHeight
-                        dataMin[3] = dataLabel
+                        dataMin[2] = dataLabelWidth
+                        dataMin[3] = dataLabelHeight
+                        dataMin[4] = dataLabel
                     end
-                    if dataMax[1] < data or dataMax[1]<0 then
+                    if dataMax[1] < data or dataMax[1] < 0 then
                         dataMax[1] = data
-                        dataMax[2] = dataLabelHeight
-                        dataMax[3] = dataLabel
+                        dataMax[2] = dataLabelWidth
+                        dataMax[3] = dataLabelHeight
+                        dataMax[4] = dataLabel
                     end
                     -- Only positive numbers are allowed for the pie chart
                     if isEmpty(data) or data < 0 then
@@ -107,7 +108,7 @@ if not DonutChart then
                         -- If we are at the middle, compute label
                         if i == tonumber(string.format('%.0f', angleToDraw / 2)) then
                             -- We need to store the label in a table to be able to draw it later properly
-                            local labelValue = tonumber(string.format('%.2f', data)) .. " "..dataUnit.."(" .. tonumber(string.format('%.2f', (data / dataSum * 100))) .. "%)"
+                            local labelValue = tonumber(string.format('%.2f', data)) .. " " .. dataUnit .. "(" .. tonumber(string.format('%.2f', (data / dataSum * 100))) .. "%)"
                             local labelWidth, labelHeight = getTextBounds(font, labelValue or '')
                             if labelWidth > biggestLabelWidth then
                                 biggestLabelWidth = labelWidth
@@ -124,85 +125,88 @@ if not DonutChart then
                 end
 
                 local lowerOccupiedY = -1
-                local leftDataX = self.x-circleRadius-biggestLabelWidth- labelWSpacing
-                local rightDataX = self.x+circleRadius+biggestLabelWidth+ labelWSpacing
+                local leftDataX = self.x - circleRadius - biggestLabelWidth - labelWSpacing
+                local rightDataX = self.x + circleRadius + biggestLabelWidth + labelWSpacing
                 -- Draw labels on the left side of the graph from the top to the bottom
-                table.sort(leftLabelList, function(a, b) return a.y < b.y end)
+                table.sort(leftLabelList, function(a, b)
+                    return a.y < b.y
+                end)
                 for i = 1, #leftLabelList do
                     local labelValue = leftLabelList[i].labelValue
                     local dataLabel = leftLabelList[i].dataLabel
                     local x = leftLabelList[i].x
                     local y = leftLabelList[i].y
-                    local height=leftLabelList[i].height
+                    local height = leftLabelList[i].height
                     local labelY = y
                     -- Is the spot taken ? if so we need to move down a bit
-                    if lowerOccupiedY>=0 and labelY-height < lowerOccupiedY then
+                    if lowerOccupiedY >= 0 and labelY - height < lowerOccupiedY then
                         labelY = lowerOccupiedY + height + labelHSpacing
                     end
                     -- Text
                     setNextTextAlign(labelLayer, AlignH_Left, AlignV_Top)
-                    addText(labelLayer, font, dataLabel,leftDataX, labelY-height)
+                    addText(labelLayer, font, dataLabel, leftDataX, labelY - height)
                     setNextTextAlign(labelLayer, AlignH_Left, AlignV_Baseline)
-                    addText(labelLayer, font, labelValue, leftDataX, labelY+height)
+                    addText(labelLayer, font, labelValue, leftDataX, labelY + height)
                     -- Lines
-                    local lineX2 = leftDataX+biggestLabelWidth+ labelWSpacing /2
+                    local lineX2 = leftDataX + biggestLabelWidth + labelWSpacing / 2
                     local lineY2 = labelY
                     addLine(labelLayer, leftDataX, lineY2, lineX2, lineY2)
                     addLine(labelLayer, lineX2, lineY2, x, y)
                     addCircle(labelLayer, x, y, 2)
-                    lowerOccupiedY = labelY+height
+                    lowerOccupiedY = labelY + height
                 end
                 -- Draw labels on the right side of the graph
                 lowerOccupiedY = -1
-                table.sort(rightLabelList, function(a, b) return a.y < b.y end)
+                table.sort(rightLabelList, function(a, b)
+                    return a.y < b.y
+                end)
                 for i = 1, #rightLabelList do
                     local labelValue = rightLabelList[i].labelValue
                     local dataLabel = rightLabelList[i].dataLabel
                     local x = rightLabelList[i].x
                     local y = rightLabelList[i].y
-                    local height=rightLabelList[i].height
-                    local width=rightLabelList[i].width
-                    local widthOffset=width/4
+                    local height = rightLabelList[i].height
+                    local width = rightLabelList[i].width
+                    local widthOffset = width / 4
                     local labelY = y
                     -- Is the spot taken ? if so we need to move down a bit
-                    if lowerOccupiedY>=0 and labelY-height < lowerOccupiedY then
+                    if lowerOccupiedY >= 0 and labelY - height < lowerOccupiedY then
                         labelY = lowerOccupiedY + height + labelHSpacing
                     end
                     -- Text
                     setNextTextAlign(labelLayer, AlignH_Right, AlignV_Top)
-                    addText(labelLayer, font, dataLabel,rightDataX+widthOffset, labelY-height)
+                    addText(labelLayer, font, dataLabel, rightDataX + widthOffset, labelY - height)
                     setNextTextAlign(labelLayer, AlignH_Right, AlignV_Baseline)
-                    addText(labelLayer, font, labelValue, rightDataX+widthOffset, labelY+height)
+                    addText(labelLayer, font, labelValue, rightDataX + widthOffset, labelY + height)
                     -- Lines
-                    local lineX2 = rightDataX+widthOffset
+                    local lineX2 = rightDataX + widthOffset
                     local lineY2 = labelY
-                    addLine(labelLayer, rightDataX-width, lineY2, lineX2, lineY2)
-                    addLine(labelLayer, rightDataX-width, lineY2, x, y)
+                    addLine(labelLayer, rightDataX - width, lineY2, lineX2, lineY2)
+                    addLine(labelLayer, rightDataX - width, lineY2, x, y)
                     addCircle(labelLayer, x, y, 2)
-                    lowerOccupiedY = labelY+height
+                    lowerOccupiedY = labelY + height
                 end
                 -- Draw pie background
-                setNextFillColor(backgroundLayer,lastColor[1], lastColor[2], lastColor[3], 1)
-                addCircle(backgroundLayer, self.x, self.y, circleRadius+2)
+                setNextFillColor(backgroundLayer, lastColor[1], lastColor[2], lastColor[3], 1)
+                addCircle(backgroundLayer, self.x, self.y, circleRadius + 2)
 
                 local mx, my = getCursor()
-                if distanceFrom(self.x, self.y, mx, my)<=circleRadius/2 then
+                if distanceFrom(self.x, self.y, mx, my) <= circleRadius / 2 then
                     --Draw central zone
-                    setNextFillColor(centralLayer,1, 1, 1, 1)
-                    addCircle(centralLayer, self.x, self.y, circleRadius/1.9)
+                    setNextFillColor(centralLayer, 1, 1, 1, 1)
+                    addCircle(centralLayer, self.x, self.y, circleRadius / 1.9)
                 end
 
                 local released = getCursorReleased()
                 if released then
                     local mx, my = getCursor()
-                    if distanceFrom(self.x, self.y, mx, my)<=circleRadius/2 then
-                        self.currentCentralMenu = self.currentCentralMenu+1
-                        if self.currentCentralMenu>4 then
+                    if distanceFrom(self.x, self.y, mx, my) <= circleRadius / 2 then
+                        self.currentCentralMenu = self.currentCentralMenu + 1
+                        if self.currentCentralMenu > 4 then
                             self.currentCentralMenu = 1
                         end
                     end
                 end
-                local mediumLabelY=self.y-circleRadius/3
                 if self.currentCentralMenu == 1 then
                     setNextTextAlign(labelLayer, AlignH_Center, AlignV_Baseline)
                     addText(labelLayer, mediumFont, "TOTAL", self.x, mediumLabelY)
@@ -212,26 +216,44 @@ if not DonutChart then
                     setNextTextAlign(labelLayer, AlignH_Center, AlignV_Baseline)
                     addText(labelLayer, mediumFont, "AVG", self.x, mediumLabelY)
                     setNextTextAlign(labelLayer, AlignH_Center, AlignV_Baseline)
-                    addText(labelLayer, mediumFont, tonumber(string.format('%.2f', dataSum/(#dataLuaArray-1))), self.x, self.y)
+                    addText(labelLayer, mediumFont, tonumber(string.format('%.2f', dataSum / (#dataLuaArray - 1))), self.x, self.y)
                 elseif self.currentCentralMenu == 3 then
                     setNextTextAlign(labelLayer, AlignH_Center, AlignV_Baseline)
                     addText(labelLayer, mediumFont, "MIN", self.x, mediumLabelY)
                     setNextTextAlign(labelLayer, AlignH_Center, AlignV_Baseline)
-                    addText(labelLayer, mediumFont, dataMin[3], self.x, self.y-dataMin[2])
+                    if dataMin[2] > circleRadius then
+                        addText(labelLayer, smallFont, dataMin[4], self.x, self.y - dataMin[3])
+                    else
+                        addText(labelLayer, mediumFont, dataMin[4], self.x, self.y - dataMin[3])
+                    end
                     setNextTextAlign(labelLayer, AlignH_Center, AlignV_Baseline)
-                    addText(labelLayer, mediumFont, tonumber(string.format('%.2f', dataMin[1])), self.x, self.y+dataMin[2])
+                    local dataValue = tonumber(string.format('%.2f', dataMin[1]))
+                    if string.len(dataValue) > 14 then
+                        addText(labelLayer, smallFont, tonumber(string.format('%.2f', dataMin[1])), self.x, self.y + dataMin[3])
+                    else
+                        addText(labelLayer, mediumFont, tonumber(string.format('%.2f', dataMin[1])), self.x, self.y + dataMin[3])
+                    end
                 elseif self.currentCentralMenu == 4 then
                     setNextTextAlign(labelLayer, AlignH_Center, AlignV_Baseline)
                     addText(labelLayer, mediumFont, "MAX", self.x, mediumLabelY)
                     setNextTextAlign(labelLayer, AlignH_Center, AlignV_Baseline)
-                    addText(labelLayer, mediumFont, dataMax[3], self.x, self.y-dataMax[2])
+                    if dataMax[2] > circleRadius then
+                        addText(labelLayer, smallFont, dataMax[4], self.x, self.y - dataMax[3])
+                    else
+                        addText(labelLayer, mediumFont, dataMax[4], self.x, self.y - dataMax[3])
+                    end
                     setNextTextAlign(labelLayer, AlignH_Center, AlignV_Baseline)
-                    addText(labelLayer, mediumFont, tonumber(string.format('%.2f', dataMax[1])), self.x, self.y+dataMax[2])
+                    local dataValue = tonumber(string.format('%.2f', dataMax[1]))
+                    if string.len(dataValue) > 14 then
+                        addText(labelLayer, smallFont, tonumber(string.format('%.2f', dataMax[1])), self.x, self.y + dataMax[3])
+                    else
+                        addText(labelLayer, mediumFont, tonumber(string.format('%.2f', dataMax[1])), self.x, self.y + dataMax[3])
+                    end
                 end
 
                 --Draw central zone
                 setNextFillColor(centralLayer, 0.001, 0, 0, 1)
-                addCircle(centralLayer, self.x, self.y, circleRadius/2)
+                addCircle(centralLayer, self.x, self.y, circleRadius / 2)
 
                 -- For now only one pie chart is supported (Size of screen is too small to read values correctly)
                 break
@@ -242,7 +264,7 @@ if not DonutChart then
             return s == nil or s == ''
         end
 
-        function distanceFrom(x1,y1,x2,y2)
+        function distanceFrom(x1, y1, x2, y2)
             return math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
         end
 
@@ -259,7 +281,7 @@ if not alreadyDeclared then
     alreadyDeclared = true
     -- We declare here our chart object only once
     -- DonutChart:new(posX, posY, circleDiameter, dataUnit)
-    -- posX and posY are coodinates of the center, dataUnit is the value type label (Use the string  "L" for fluids for example) 
+    -- posX and posY are coordinates of the center, dataUnit is the value type label (Use the string  "L" for fluids for example)
     -- Disclaimer : The diameter the chart does not include labels, in order to see the labels on the side, do not use the entire screen width as diameter)
     testPie = DonutChart:new(rx / 2, ry / 2, 400, "kg")
 end
@@ -267,7 +289,7 @@ end
 -- In this example, i draw a static chart, the data is a table with the first column containing data types and second colum data value
 -- First line is my chartName, first colum is my data label
 -- You can also feed the chart with dynamic data using the getInput() method
-local testData = { { "", "My ore stock" }, { "Data 1", 1 }, { "Data 2", 10 },{ "Data 3", 10 },{ "Data 4", 10 },{ "Data 5", 10 },{ "Data 6", 10 },{ "Data 7", 10 },{ "Data 8", 10 },{ "Data 9", 10 },}
+local testData = { { "", "My ore stock" }, { "Data 1", 1 }, { "Data 2", 10 }, { "Data 3", 10 }, { "Data 4", 10 }, { "Data 5", 10 }, { "Data 6", 10 }, { "Data 7", 10 }, { "Data 8", 10 }, { "Data 9", 10 }, }
 testPie:draw(testData)
 
 -- Note that if you don't need real time chart data you can remove this line, a simple mouse click will refresh the chart
