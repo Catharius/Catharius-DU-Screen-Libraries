a61af1e6ee0fe47078fd773646a4d6f4d6c0a867
Catharius <72415020+Catharius@users.noreply.github.com>
2022-07-17 22:45:23 +0200

    Update onStart2.lua

--- gauges/hudExample/library/onStart2.lua
+++ gauges/hudExample/library/onStart2.lua
@@ -10,3 +10,23 @@ function getFuelLevel(fueltank)
     end
     return fuelpercentage
 end
+
+function getSpeed(coordinates)
+    local locVelVec = vec3(coordinates)
+    local locVelVecLen = locVelVec:len()
+    local speedValue = 0
+    if locVelVecLen > 0.3 then
+        local locVelVecN = locVelVec:normalize()
+        local rY, rX = 0,0
+        rY = -math.asin(locVelVecN.x)
+        rX = -math.asin(locVelVecN.z)
+
+        if locVelVec.y < 0 then
+            rY = (rY < 0 and -1 or 1) * math.pi - rY
+        end
+
+        local hPos = locVelVecN*1700
+        speedValue = locVelVecLen*3.6
+    end
+    return speedValue
+end
